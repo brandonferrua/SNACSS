c3979800c88ecf90e00fbbaa6090a10c35df78d6
Brandon Ferrua <bferrua@curse.com>
2013-03-27 09:54:49 -0700

    Setup proper includes for templating

---
+++ includes/_nav.php
@@ -0,0 +1,16 @@
+<nav>
+    <ul>
+        <li>
+            <a href="#">Nav Item</a>
+        </li>
+        <li>
+            <a href="#">Nav Item</a>
+        </li>
+        <li>
+            <a href="#">Nav Item</a>
+        </li>
+        <li>
+            <a href="#">Nav Item</a>
+        </li>
+    </ul>   
+</nav>
---
+++ includes/functions/_global.php
@@ -0,0 +1,9 @@
+<?php
+    $live = "false";
+    $imageURL_local = "";
+    if ($live == "false") {
+        $imageURL_local = "/stylyy";
+    } else {
+        $imageURL_local = "";
+    }
+?>
