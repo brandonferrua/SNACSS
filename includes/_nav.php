6011631569e34e669155d0702c89a0d99e45507b
Brandon Ferrua <Dougie@Brandon-Ferruas-iMac.local>
2013-04-19 13:04:23 -0700

    added font-awesome icons

--- includes/_nav.php
+++ includes/_nav.php
@@ -1,13 +1,13 @@
 <nav role="navigation">
-    <ul>
-        <li>
+    <ul class="p-nav p-nav-b">
+        <li class="p-nav-item nav-sub">
             <a href="#">Style Guide</a>
             <ul class="b-list-b">
                 <li class="b-list-item"><a href="#">What is this?</a></li>
                 <li class="b-list-item"><a href="#">Customize</a></li>
             </ul>
         </li>
-        <li>
+        <li class="p-nav-item nav-sub">
             <a href="#">Documentation</a>
             <ul class="b-list-b">
                 <li class="b-list-item"><a href="#">Get Started</a></li>
@@ -17,10 +17,10 @@
                 <li class="b-list-item"><a href="#">Interactions</a></li>
             </ul>
         </li>
-        <li>
+        <li class="p-nav-item">
             <a href="#">About this project</a>
         </li>
-        <li class="cc">
+        <li class="p-nav-item cc">
             <a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed.en_US">Creative Commons Attribution 3.0 Unported License</a>.
         </li>
     </ul>   
